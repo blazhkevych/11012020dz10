79c242dd679c6992ff19dffa41f3fb5562188648
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:14:40 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz10.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры два числа.Посчитать 	их сумму, произведение и среднее 	арифметическое.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	double a, b;
+	cout << "Введите два числа : ";
+	cin >> a >> b;
+	cout << "Сумма = " << a + b << endl;
+	cout << "Произведение = " << a * b << endl;
+	cout << "Среднее арифметическое = " << (a + b) / 2 << endl;
+}
